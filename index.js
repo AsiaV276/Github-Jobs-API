e71d9dddcd724169f1201107e2a66840bcf17a2a
Asia Valentine <asiavalentine22@gmail.com>
2020-10-14 17:34:33 -0400

    theme toggle

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+//const fetch = require('node-fetch');
 const PORT = process.env.PORT || 5000
 const app = express();
 //require('dotenv').config();
@@ -6,3 +7,10 @@ const app = express();
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 app.use(express.static('public'));
 
+//app.get('/:search?', async (req, res) => {
+    /*console.log(req.params);
+    const search = req.params;
+    var api_url = `https://jobs.github.com/positions.json?search=${search}`
+    const response = await fetch(api_url);
+    res.json(json);*/
+//})
---
+++ public/toggleTheme.js
@@ -0,0 +1,42 @@
+checkBox.checked = true;
+
+function changeDarkMode() {
+    //var checkBox = document.getElementById('toggle-dark-mode');
+    console.log('theme changed');
+    /* selecting elements */
+    /*
+    var toggleBkg = document.getElementsByClassName('toggle-switch');
+    var socialBox = document.getElementsByClassName('social-box');
+    var igBox = document.getElementsByClassName('ig-border-inner');
+    var overviewBox = document.getElementsByClassName('overview-box');
+    var background = document.body;
+
+
+        //changing toggle switch background
+        toggleBkg[0].classList.toggle('light-toggle-switch');
+
+        //changing social boxes
+        var i;
+        for (i = 0; i < socialBox.length; i++) {
+            socialBox[i].classList.toggle('light-social-box');
+        }
+
+        //changing ig background box
+        igBox[0].classList.toggle('light-social-box');
+
+        //changing overview boxes
+        var x;
+        for (x = 0; x < overviewBox.length; x++) {
+            overviewBox[x].classList.toggle('light-overview-box');
+        }
+        
+        //changing background
+        background.classList.toggle('light-body');
+
+        
+
+    */
+    
+}
+
+changeDarkMode();
